63d1ae70d14da1706f052af56a52943916a1f456
VytenisDom <vytenis.domasevicius@vka.lt>
2019-06-23 00:39:18 +0300

    Rewritten the server in express, allowed CORS, now handles both get and post requests, UI changes

--- app.js
+++ app.js
@@ -1,33 +1,78 @@
 const http = require('http');
-//const script = require('./webapp/index.js');
-const fs = require('fs');
-var nStatic = require('node-static');
-var fileServer = new nStatic.Server('./public');
+const nStatic = require('node-static');
+const mysql = require('mysql');
+const fileServer = new nStatic.Server('./public');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const port = 3000;
 
-// Main server function
-const server = http.createServer(function(req, res){
-    // Specifying the content type of our data for browser to understand that it's html and parse it
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    // Checking if the required file is present
-    fs.readFile('public/webapp/index.html', function(err, data){
-        if(err){
-            res.WriteHead(404);
-            res.write('Error: File not found.');
+// Telling the server to use the JSON bodyparser module when handling post requests
+app.use(bodyParser.json());
+app.use(express.json());
+
+// Allowing cross-origin requests to your api url
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const connection = mysql.createConnection({
+    // Connection properties
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'registrations'
+});
+
+// MySQL connection
+connection.connect(function(err){
+    if(err){
+        console.log('Failed to connect to the database.');
+    } else {
+        console.log("Succesfully connected to the database.")
+    }
+})
+
+// Serving the home page when the request is '/'
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/webapp/index.html');
+});
+
+// Receiving a post request from '/postreq'. Parsing the JSON body and inserting into the database.
+app.post('/postreq', function (req, res) {
+    console.log("INSERT INTO appregistrations (FirstName, LastName, Date, Time) VALUES( '"
+        + req.body.FirstName + "', "
+        + "'" + req.body.LastName + "', "
+        + "'" + req.body.Date + "', "
+        + "'" + req.body.Time + ")");
+    connection.query("INSERT INTO appregistrations (FirstName, LastName, Date, Time) VALUES('" 
+    + req.body.FirstName + "', " 
+    + "'" + req.body.LastName + "', "
+    + "'" + req.body.Date + "', " 
+    + "'" + req.body.Time + "' )" , function (err) {
+        if (err) {
+            console.log("Error : Bad query declaration.");
         } else {
-            // Good to go, serve the website
-            res.write(data);
+            console.log("Inserting succesful.");
         }
-        res.end();
     });
 });
 
-// Function for listening on specified port
-server.listen(port, function(err){
-    if(err){
-        console.log("Ooops! Something went wrong!");
-    } else {
-        // Everythings fine, starting the server
-        console.log('Server is listening on port ' + port);
-    }
+// Serving the DB info with api call at '/getreservations'
+app.get('/getreservations', function (req, res) {
+    connection.query("SELECT * FROM appregistrations", function (err, rows, fields) {
+        if (err) {
+            console.log("Error : Bad query declaration.");
+        } else {
+            console.log("Selection succesful.");
+            res.json(rows);
+        }
+    });
+});
+
+// Listening on port 3000
+app.listen(port, function () {
+    console.log('Server is listening on port ' + port);
 });
